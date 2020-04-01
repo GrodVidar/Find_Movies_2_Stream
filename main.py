fcdeaf5895633d5eec048dcffc692ccba54fa2ed
GrodVidar <vexxart@gmail.com>
2020-04-01 21:05:06 +0200

    added image to movie searched for

--- main.py
+++ main.py
@@ -10,9 +10,12 @@ app = flask.Flask(__name__)
 
 def get_providers(resp):
     names = {}
+    img = ''
     if resp and 'results' in resp:
         for j in range(len(resp['results'])):
             name = resp['results'][j]['name']
+            if j == 0:
+                img = resp['results'][j]['picture']
             if 'locations' in resp['results'][j]:
                 names[name] = []
                 for i in range(len(resp['results'][j]['locations'])):
@@ -20,7 +23,7 @@ def get_providers(resp):
                         names[name].append(resp['results'][j]['locations'][i]['display_name'])
             else:
                 return "!"
-        return names
+        return names, img
     else:
         return "!"
 
@@ -28,6 +31,7 @@ def get_providers(resp):
 def viaplay_finder(name):
     resp = requests.get("https://content.viaplay.se/pcdash-se/search?query="+name)
     if resp:
+        img = ''
         json_resp = resp.json()
         if '_embedded' in json_resp:
             if json_resp['_embedded']['viaplay:blocks'][0]['_embedded']['viaplay:products']:
@@ -35,6 +39,8 @@ def viaplay_finder(name):
                 movies = {}
                 for i in range(len(products)):
                     movie = products[i]['_links']['self']['title']
+                    if i == 0 :
+                        img = products[i]['content']['images']['boxart']['url']
                     movies[movie] = {}
                     if 'tvod' in products[i]['system']['availability']:
                         movies[movie]['rent_price'] = products[i]['system']['availability']['tvod']['planInfo']['price']
@@ -42,14 +48,15 @@ def viaplay_finder(name):
                         movies[movie]['purchase_price'] = products[i]['system']['availability']['est']['planInfo']['price']
                     if 'svod' in products[i]['system']['availability']:
                         movies[movie]['is_streamable'] = True
-                return movies
+                return movies, img
     return None
 
 
 def find(title, params):
     with open("amount.txt", "r") as file:
         amount = int(file.readline())
-    if amount < 1000 and title and len(title) > 1:
+    image = ''
+    if amount < 1000 and title and len(title) > 1 and params['telly']:
         url = "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup"
         querystring = {"term": title, "country": "se"}
         headers = {
@@ -59,9 +66,9 @@ def find(title, params):
         response = requests.request("GET", url, headers=headers, params=querystring)
         with open("amount.txt", "w") as file:
             file.write(str(amount + 1))
-        movies = get_providers(response.json())
+        movies, image = get_providers(response.json())
         movies_list = []
-        if movies != '!' and params['telly']:
+        if movies != '!':
             for i in movies:
                 movies_string = ''
                 movies_string += f"{i} can be seen at"
@@ -71,32 +78,32 @@ def find(title, params):
                 movies_string = movies_string[:-1]
                 if movies_string != f"{i} can be seen a":
                     movies_list.append(movies_string)
-        if 'Viaplay' in params[title]:
-            via_movies = viaplay_finder(title)
-            if via_movies:
-                for i in via_movies:
-                    is_and = False
-                    movies_string = f"{i} can be "
-                    if 'is_streamable' in via_movies[i]:
-                        if is_and:
-                            movies_string += '& '
-                        movies_string += "streamed "
-                        is_and = True
-                    if 'rent_price' in via_movies[i]:
-                        if is_and:
-                            movies_string += '& '
-                        movies_string += f"rented for {via_movies[i]['rent_price']} SEK "
-                        is_and = True
-                    if 'purchase_price' in via_movies[i]:
-                        if is_and:
-                            movies_string += '& '
-                        movies_string += f"purchased for {via_movies[i]['purchase_price']} SEK "
-                    movies_string += "at Viaplay"
-                    movies_list.append(movies_string)
-        print(movies_list)
-        return movies_list
     else:
         return "free quota exceeded or name too short"
+    if 'Viaplay' in params[title]:
+        via_movies, image = viaplay_finder(title)
+        if via_movies:
+            for i in via_movies:
+                is_and = False
+                movies_string = f"{i} can be "
+                if 'is_streamable' in via_movies[i]:
+                    if is_and:
+                        movies_string += '& '
+                    movies_string += "streamed "
+                    is_and = True
+                if 'rent_price' in via_movies[i]:
+                    if is_and:
+                        movies_string += '& '
+                    movies_string += f"rented for {via_movies[i]['rent_price']} SEK "
+                    is_and = True
+                if 'purchase_price' in via_movies[i]:
+                    if is_and:
+                        movies_string += '& '
+                    movies_string += f"purchased for {via_movies[i]['purchase_price']} SEK "
+                movies_string += "at Viaplay"
+                movies_list.append(movies_string)
+    print(movies_list)
+    return movies_list, image
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -121,7 +128,8 @@ def home():
             my_dict['telly'] = True
         if flask.request.form.get('Viaplay'):
             my_dict[title].append('Viaplay')
-        return flask.render_template('index.html', title=title, info=find(title, my_dict))
+        info, image = find(title, my_dict)
+        return flask.render_template('index.html', title=title, info=info, image=image)
 
 
 if __name__ == '__main__':
